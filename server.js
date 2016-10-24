8e8c3564fc4bedb7ca267bcd254cb1c3b533746c
ryan1002 <ryanmodeste2@gmail.com>
2016-10-24 15:41:30 +0100

    gulp + sass installed

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app     = express();
 const path    = require("path");
 const port    = process.env.PORT || 8000;
 
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req,res) => {
   return res.sendFile(path.join(__dirname, "index.html"));
 });
-
 
 app.listen(port, () =>{
   console.log("Server has started on port " + port);
