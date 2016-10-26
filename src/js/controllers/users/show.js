3ce33200dd92338d96ea3175a249763ec044ea62
ryan1002 <ryanmodeste2@gmail.com>
2016-10-26 10:18:37 +0100

    calender now showing on users show

--- src/js/controllers/users/show.js
+++ src/js/controllers/users/show.js
@@ -5,5 +5,19 @@ angular
 usersShowCtrl.$inject = ["User", "$stateParams"];
 function usersShowCtrl(User, $stateParams) {
   const vm = this;
-  vm.user = User.get($stateParams);
+
+  vm.calendarView = 'month';
+  vm.viewDate = new Date();
+
+  User
+    .get($stateParams)
+    .$promise
+    .then(data => {
+      vm.user = data;
+      vm.user.events.map((event) => {
+        event.startsAt = new Date(event.startsAt);
+        event.endsAt = new Date(event.endsAt);
+        return event;
+      });
+    });
 }
